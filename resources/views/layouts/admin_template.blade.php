51dca26a17da03c19e9113c8e73ac35f1bb5ee32
kevinpauneljohn <johnkevinpaunel@gmail.com>
2019-08-15 20:41:10 +0800

    change the crm company logo

--- resources/views/layouts/admin_template.blade.php
+++ resources/views/layouts/admin_template.blade.php
@@ -41,7 +41,7 @@
         <!-- Logo -->
         <a href="index2.html" class="logo">
             <!-- mini logo for sidebar mini 50x50 pixels -->
-            <span class="logo-mini"><b>A</b>LT</span>
+            <span class="logo-mini"><b>O</b>Box</span>
             <!-- logo for regular state and mobile devices -->
             <span class="logo-lg"><b>Outerbox</b>PRO</span>
         </a>
