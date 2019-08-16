8e3c764c37a1df6c7fbbcc26938cf10f86bb2c24
kevinpauneljohn <johnkevinpaunel@gmail.com>
2019-08-17 01:51:02 +0800

    updated EmployeeController

--- app/Http/Controllers/Employee/EmployeeController.php
+++ app/Http/Controllers/Employee/EmployeeController.php
@@ -13,28 +13,27 @@ class EmployeeController extends Controller
     {
         $validator = Validator::make($request->all(),[
             'firstname'     => 'required|min:2|max:50',
-//            'lastname'      => 'required|min:2|max:50',
-//            'email'         => 'required|email|unique:users,email',
-//            'username'      => 'required|unique:users,username',
-//            'password'      => 'required|min:3|max:50|confirmed'
+            'lastname'      => 'required|min:2|max:50',
+            'email'         => 'required|email|unique:users,email',
+            'username'      => 'required|unique:users,username',
+            'password'      => 'required|min:3|max:50|confirmed'
         ]);
 //
         if($validator->passes())
         {
             $user = new User;
-            $user->firstname = 'kevin';
-            $user->save();
-//            $user->middlename = $request->middlename;
-//            $user->lastname = $request->lastname;
-//            $user->email = $request->email;
-//            $user->username = $request->username;
-//            $user->password = bcrypt($request->password);
-//            $user->assignRole($request->role);
+            $user->firstname = $request->firstname;
+            $user->middlename = $request->middlename;
+            $user->lastname = $request->lastname;
+            $user->email = $request->email;
+            $user->username = $request->username;
+            $user->password = bcrypt($request->password);
+            $user->assignRole($request->role);
 
-//            $message = ($user->save()) ? ['success' => true] : ['success' => false];
-//            return response()->json($message);
+            $message = ($user->save()) ? ['success' => true] : ['success' => false];
+            return response()->json($message);
         }
-//
-//        return response()->json($validator->errors());
+
+        return response()->json($validator->errors());
     }
 }
