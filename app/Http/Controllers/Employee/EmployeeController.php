610b35271cc294c7c8ca74f9c951ee55c507fca4
kevinpauneljohn <johnkevinpaunel@gmail.com>
2019-08-17 00:47:15 +0800

    edit employeecontroller and employee js

--- app/Http/Controllers/Employee/EmployeeController.php
+++ app/Http/Controllers/Employee/EmployeeController.php
@@ -11,29 +11,31 @@ class EmployeeController extends Controller
 {
     public function addEmployee(Request $request)
     {
-        $validator = Validator::make($request->all(),[
-            'firstname'     => 'required|min:2|max:50',
-            'lastname'      => 'required|min:2|max:50',
-            'email'         => 'required|email|unique:users,email',
-            'username'      => 'required|unique:users,username',
-            'password'      => 'required|min:5|max:50|confirmed'
-        ]);
+//        $validator = Validator::make($request->all(),[
+//            'firstname'     => 'required|min:2|max:50',
+//            'lastname'      => 'required|min:2|max:50',
+//            'email'         => 'required|email|unique:users,email',
+//            'username'      => 'required|unique:users,username',
+//            'password'      => 'required|min:5|max:50|confirmed'
+//        ]);
+//
+//        if($validator->passes())
+//        {
+//            $user = new User;
+//            $user->firstname = $request->firstname;
+//            $user->middlename = $request->middlename;
+//            $user->lastname = $request->lastname;
+//            $user->email = $request->email;
+//            $user->username = $request->username;
+//            $user->password = bcrypt($request->password);
+//            $user->assignRole($request->role);
+//
+//            $message = ($user->save()) ? ['success' => true] : ['success' => false];
+//            return $message;
+//        }
+//
+//        return response()->json($validator->errors());
 
-        if($validator->passes())
-        {
-            $user = new User;
-            $user->firstname = $request->firstname;
-            $user->middlename = $request->middlename;
-            $user->lastname = $request->lastname;
-            $user->email = $request->email;
-            $user->username = $request->username;
-            $user->password = bcrypt($request->password);
-            $user->assignRole($request->role);
-
-            $message = ($user->save()) ? ['success' => true] : ['success' => false];
-            return $message;
-        }
-
-        return response()->json($validator->errors());
+        return 'kevin pauenl';
     }
 }
