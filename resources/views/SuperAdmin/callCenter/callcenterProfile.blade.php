2dc10aa84699635a8f1936bf8f4c9e1fa3a5273f
kevinpauneljohn <johnkevinpaunel@gmail.com>
2019-08-19 20:21:09 +0800

    updated the call center profile page

--- resources/views/SuperAdmin/callCenter/callcenterProfile.blade.php
+++ resources/views/SuperAdmin/callCenter/callcenterProfile.blade.php
@@ -87,8 +87,8 @@
                                 <td>{{ucfirst($employee->role_name)}}</td>
                                 <td>
                                     <a href="#"><button type="button" class="btn btn-success edit-btn" title="View"><i class="fa fa-eye"></i></button></a>
-                                    <button type="button" class="btn btn-primary edit-callcenter" title="Edit" data-toggle="modal" data-target="#edit_callCenterModal" value=""><i class="fa fa-edit"></i></button>
-                                    <button type="button" class="btn btn-danger delete-callcenter" title="Delete" data-toggle="modal" data-target="#delete_call_center" value=""><i class="fa fa-trash"></i></button>
+                                    <button type="button" class="btn btn-primary edit-employee" title="Edit" data-toggle="modal" data-target="#edit_employee" value="{{$employee->id}}"><i class="fa fa-edit"></i></button>
+                                    <button type="button" class="btn btn-danger delete-employee-btn" title="Delete" data-toggle="modal" data-target="#delete_employee" value="{{$employee->id}}"><i class="fa fa-trash"></i></button>
                                 </td>
                             </tr>
                         @endforeach
@@ -217,6 +217,113 @@
             </form>
         </div>
     </div>
+
+    {{--Edit Staff--}}
+    <div class="modal fade" id="edit_employee">
+        <div class="modal-dialog modal-lg">
+            <form method="post" id="update-staff">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span></button>
+                        <h4 class="modal-title">Update Employee</h4>
+                    </div>
+
+                    <div class="modal-body">
+                        @csrf
+                        <input type="hidden" name="user_value" id="user_value"/>
+                        <div class="form-group">
+                            <div class="row">
+                                <span class="col-lg-4">
+                                    <div class="form-group">
+                                        <div class="edit_firstname">
+                                            <label for="edit_firstname">First name</label> <span class="required">*</span>
+                                            <input type="text" name="edit_firstname" id="edit_firstname" class="form-control"/>
+                                        </div>
+                                    </div>
+                                </span>
+                                <span class="col-lg-4">
+                                    <div class="form-group">
+                                        <div class="edit_middlename">
+                                            <label for="edit_middlename">Middle name</label>
+                                            <input type="text" name="edit_middlename" id="edit_middlename" class="form-control"/>
+                                        </div>
+                                    </div>
+                                </span>
+                                <span class="col-lg-4">
+                                    <div class="form-group">
+                                        <div class="edit_lastname">
+                                            <label for="edit_lastname">Last name</label><span class="required">*</span>
+                                            <input type="text" name="edit_lastname" id="edit_lastname" class="form-control"/>
+                                        </div>
+                                    </div>
+                                </span>
+                            </div>
+                            <div class="row">
+                                <span class="col-lg-6">
+                                    <div class="form-group">
+                                        <div class="edit_email">
+                                            <label for="edit_email">Email</label><span class="required">*</span>
+                                            <input type="text" name="edit_email" id="edit_email" class="form-control"/>
+                                        </div>
+                                    </div>
+                                </span>
+                                <span class="col-lg-6">
+                                    <div class="form-group">
+                                            <div class="edit_role">
+                                                <label for="edit_role">Select Role</label>
+                                                <select name="edit_role" class="form-control role" id="edit_role">
+                                                    <option></option>
+                                                    @foreach($roles as $role)
+                                                        <option value="{{$role->name}}">{{ucfirst($role->name)}}</option>
+                                                    @endforeach
+                                                </select>
+                                            </div>
+                                        </div>
+                                </span>
+                            </div>
+
+                            <input type="hidden" name="old_role" id="old_role"/>
+                            <input type="hidden" name="edit_callcenter" value="{{$cc_id}}">
+                        </div>
+
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button>
+                            <button type="submit" class="btn bg-purple"><i class="fa fa-check"></i> Save</button>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+
+
+    <div class="modal modal-danger fade" id="delete_employee">
+        <div class="modal-dialog">
+            <form method="post" id="delete-staff">
+                @csrf
+
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span></button>
+                        <h4 class="modal-title">Delete Role</h4>
+                    </div>
+                    <div class="modal-body">
+                        <span>Are you sure you want to delete User: <b class="user_name"></b>?</span>
+                    </div>
+                    <input type="hidden" name="user_delete" class="user_delete"/>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-outline pull-left" data-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-outline" name="submit" value=""><i class="fa fa-trash"></i> Delete</button>
+                    </div>
+                </div>
+            </form>
+            <!-- /.modal-content -->
+        </div>
+        <!-- /.modal-dialog -->
+    </div>
+    <!-- /.modal -->
 @endsection
 
 @section('extra_script')
@@ -229,6 +336,7 @@
     <!-- FastClick -->
     <script src="{{asset('/bower_components/fastclick/lib/fastclick.js')}}"></script>
 
+    <script src="{{asset('/js/callcenter.profile.js')}}"></script>
     <script src="{{asset('/js/employee.js')}}"></script>
 
     <!-- growl notification -->
