0f90ea6e86a4dc391c537c9b97e86993f6377e7f
kevinpauneljohn <johnkevinpaunel@gmail.com>
2019-08-20 20:59:48 +0800

    updated the admin dashboard

--- app/Http/Controllers/EmployeePageController.php
+++ app/Http/Controllers/EmployeePageController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Spatie\Permission\Models\Role;
 
 
 class EmployeePageController extends Controller
@@ -18,20 +19,25 @@ class EmployeePageController extends Controller
     {
         $user_id = Auth::user()->id;
         $assignedCallCenter = DB::table('callcenterdetails')->where('user_id','=',$user_id)->select('cc_id')->get();
+        $role = Role::all()->except(['1']);
 
         $employees = DB::table('users')
             ->leftJoin('callcenterdetails','users.id','=','callcenterdetails.user_id')
-            ->select('users.*')
+            ->leftJoin('model_has_roles','users.id','=','model_has_roles.model_id')
+            ->leftJoin('roles','model_has_roles.role_id','=','roles.id')
+            ->select('users.id','users.firstname','users.middlename','users.lastname','users.email','users.username','users.created_at','roles.name as role_name','callcenterdetails.cc_id as cc_id')
             ->where([
                 ['callcenterdetails.cc_id','=',$assignedCallCenter[0]->cc_id],
-                ['users.id','!=',$user_id]
+                ['users.id','!=',$user_id],
+                ['users.deleted_at','=',null]
             ])
             ->get();
 
         return view('Employee.agent')->with([
             'user_id' => $user_id,
-            'assignedCallCenter' => $assignedCallCenter,
-            'employees' => $employees
+            'cc_id' => $assignedCallCenter[0]->cc_id,
+            'employees' => $employees,
+            'roles' => $role
         ]);
     }
 
--- resources/views/Employee/agent.blade.php
+++ resources/views/Employee/agent.blade.php
@@ -13,7 +13,7 @@
     <link href="//cdnjs.cloudflare.com/ajax/libs/animate.css/3.2.0/animate.min.css" rel="stylesheet">
 @endsection
 @section('page_header')
-    Agent
+    Agent <button type="button" class="btn bg-purple" data-toggle="modal" data-target="#create-staff"><i class="fa fa-plus"></i> Add New</button>
 @endsection
 @section('main_content')
     <div class="box">
@@ -26,7 +26,6 @@
                     <th>Email</th>
                     <th>Username</th>
                     <th>Role</th>
-                    <th>Call Center</th>
                     <th width="15%">Action</th>
                 </tr>
                 </thead>
@@ -39,10 +38,9 @@
                         </td>
                         <td>{{$employee->email}}</td>
                         <td>{{$employee->username}}</td>
-                        <td></td>
-                        <td></td>
+                        <td>{{ucfirst($employee->role_name)}}</td>
                         <td>
-                            <a href="{{url('/employee/profile/'.$employee->id)}}"><button type="button" class="btn btn-success edit-btn" title="View"><i class="fa fa-eye"></i></button></a>
+                            <a href="#"><button type="button" class="btn btn-success edit-btn" title="View"><i class="fa fa-eye"></i></button></a>
                             <button type="button" class="btn btn-primary edit-employee" title="Edit" data-toggle="modal" data-target="#edit_employee" value="{{$employee->id}}"><i class="fa fa-edit"></i></button>
                             <button type="button" class="btn btn-danger delete-employee-btn" title="Delete" data-toggle="modal" data-target="#delete_employee" value="{{$employee->id}}"><i class="fa fa-trash"></i></button>
                         </td>
@@ -56,7 +54,6 @@
                     <th>Email</th>
                     <th>Username</th>
                     <th>Role</th>
-                    <th>Call Center</th>
                     <th width="15%">Action</th>
                 </tr>
                 </tfoot>
@@ -65,6 +62,220 @@
         <!-- /.box-body -->
     </div>
     <!-- /.box -->
+
+    <div class="modal fade" id="create-staff">
+        <div class="modal-dialog modal-lg">
+            <form method="post" id="add-staff">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span></button>
+                        <h4 class="modal-title">Add New Employee</h4>
+                    </div>
+
+                    <div class="modal-body">
+                        @csrf
+
+                        <div class="form-group">
+                            <div class="row">
+                                <span class="col-lg-4">
+                                    <div class="form-group">
+                                        <div class="firstname">
+                                            <label for="firstname">First name</label> <span class="required">*</span>
+                                            <input type="text" name="firstname" id="firstname" class="form-control"/>
+                                        </div>
+                                    </div>
+                                </span>
+                                <span class="col-lg-4">
+                                    <div class="form-group">
+                                        <div class="middlename">
+                                            <label for="middlename">Middle name</label>
+                                            <input type="text" name="middlename" id="middlename" class="form-control"/>
+                                        </div>
+                                    </div>
+                                </span>
+                                <span class="col-lg-4">
+                                    <div class="form-group">
+                                        <div class="lastname">
+                                            <label for="lastname">Last name</label><span class="required">*</span>
+                                            <input type="text" name="lastname" id="lastname" class="form-control"/>
+                                        </div>
+                                    </div>
+                                </span>
+                            </div>
+                            <div class="row">
+                                <span class="col-lg-6">
+                                    <div class="form-group">
+                                        <div class="email">
+                                            <label for="email">Email</label><span class="required">*</span>
+                                            <input type="text" name="email" id="email" class="form-control"/>
+                                        </div>
+                                    </div>
+                                </span>
+                                <span class="col-lg-6">
+                                    <div class="form-group">
+                                        <div class="username">
+                                            <label for="username">Username</label><span class="required">*</span>
+                                            <input type="text" name="username" id="username" class="form-control"/>
+                                        </div>
+                                    </div>
+                                </span>
+                            </div>
+
+                            <div class="row">
+                                <span class="col-lg-6">
+                                    <div class="form-group">
+                                        <div class="password">
+                                            <label for="password">Password</label><span class="required">*</span>
+                                            <input type="password" name="password" id="password" class="form-control"/>
+                                        </div>
+                                    </div>
+                                </span>
+                                <span class="col-lg-6">
+                                    <div class="form-group">
+                                        <div class="password_confirmation">
+                                            <label for="password_confirmation">Confirm Password</label><span class="required">*</span>
+                                            <input type="password" name="password_confirmation" id="password_confirmation" class="form-control"/>
+                                        </div>
+                                    </div>
+                                </span>
+                            </div>
+
+                            <div class="form-group">
+                                <div class="row">
+                                    <div class="col-lg-4">
+                                        <div class="form-group">
+                                            <div class="role">
+                                                <label for="role">Select Role</label>
+                                                <select name="role" class="form-control role" id="role">
+                                                    <option></option>
+                                                    @foreach($roles as $role)
+                                                        <option value="{{$role->name}}">{{ucfirst($role->name)}}</option>
+                                                    @endforeach
+                                                </select>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <input type="hidden" name="callcenter" value="{{$cc_id}}"/>
+                        </div>
+
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button>
+                            <button type="submit" class="btn bg-purple"><i class="fa fa-check"></i> Save</button>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+
+    {{--Edit Staff--}}
+    <div class="modal fade" id="edit_employee">
+        <div class="modal-dialog modal-lg">
+            <form method="post" id="update-staff">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span></button>
+                        <h4 class="modal-title">Update Employee</h4>
+                    </div>
+
+                    <div class="modal-body">
+                        @csrf
+                        <input type="hidden" name="user_value" id="user_value"/>
+                        <div class="form-group">
+                            <div class="row">
+                                <span class="col-lg-4">
+                                    <div class="form-group">
+                                        <div class="edit_firstname">
+                                            <label for="edit_firstname">First name</label> <span class="required">*</span>
+                                            <input type="text" name="edit_firstname" id="edit_firstname" class="form-control"/>
+                                        </div>
+                                    </div>
+                                </span>
+                                <span class="col-lg-4">
+                                    <div class="form-group">
+                                        <div class="edit_middlename">
+                                            <label for="edit_middlename">Middle name</label>
+                                            <input type="text" name="edit_middlename" id="edit_middlename" class="form-control"/>
+                                        </div>
+                                    </div>
+                                </span>
+                                <span class="col-lg-4">
+                                    <div class="form-group">
+                                        <div class="edit_lastname">
+                                            <label for="edit_lastname">Last name</label><span class="required">*</span>
+                                            <input type="text" name="edit_lastname" id="edit_lastname" class="form-control"/>
+                                        </div>
+                                    </div>
+                                </span>
+                            </div>
+                            <div class="row">
+                                <span class="col-lg-6">
+                                    <div class="form-group">
+                                        <div class="edit_email">
+                                            <label for="edit_email">Email</label><span class="required">*</span>
+                                            <input type="text" name="edit_email" id="edit_email" class="form-control"/>
+                                        </div>
+                                    </div>
+                                </span>
+                                <span class="col-lg-6">
+                                    <div class="form-group">
+                                            <div class="edit_role">
+                                                <label for="edit_role">Select Role</label>
+                                                <select name="edit_role" class="form-control role" id="edit_role">
+                                                    <option></option>
+                                                    @foreach($roles as $role)
+                                                        <option value="{{$role->name}}">{{ucfirst($role->name)}}</option>
+                                                    @endforeach
+                                                </select>
+                                            </div>
+                                        </div>
+                                </span>
+                            </div>
+
+                            <input type="hidden" name="old_role" id="old_role"/>
+                            <input type="hidden" name="edit_callcenter" value="{{$cc_id}}">
+                        </div>
+
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button>
+                            <button type="submit" class="btn bg-purple"><i class="fa fa-check"></i> Save</button>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+
+    <div class="modal modal-danger fade" id="delete_employee">
+        <div class="modal-dialog">
+            <form method="post" id="delete-staff">
+                @csrf
+
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span></button>
+                        <h4 class="modal-title">Delete Role</h4>
+                    </div>
+                    <div class="modal-body">
+                        <span>Are you sure you want to delete User: <b class="user_name"></b>?</span>
+                    </div>
+                    <input type="hidden" name="user_delete" class="user_delete"/>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-outline pull-left" data-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-outline" name="submit" value=""><i class="fa fa-trash"></i> Delete</button>
+                    </div>
+                </div>
+            </form>
+            <!-- /.modal-content -->
+        </div>
+        <!-- /.modal-dialog -->
+    </div>
+    <!-- /.modal -->
 @endsection
 
 @section('extra_script')
