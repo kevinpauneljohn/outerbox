5377bbb344a803e42fed43ca431f70a5343d0536
john kevin paunel <johnkevinpaunel@gmail.com>
2019-08-29 21:00:40 +0800

    updated the call center page address call

--- app/Http/Controllers/Leads/LeadsController.php
+++ app/Http/Controllers/Leads/LeadsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Leads;
 
+use App\Models\Lead;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -9,6 +10,14 @@ class LeadsController extends Controller
 {
     public function get_response(Request $request)
     {
-        return $request->all();
+        $app_user_id = $request->input('app_user_id');
+        $app_response = $request->input('app_response');
+
+        $leads = new Lead;
+        $leads->app_user_id = $app_user_id;
+        $leads->app_response = $app_response;
+        $leads->save();
+
+        return response()->json(['success' => true]);
     }
 }
