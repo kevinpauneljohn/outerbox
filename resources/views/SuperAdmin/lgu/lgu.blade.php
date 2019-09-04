57158d21ba1c5cf8a0aa79df08aa74d87b7027ea
john kevin paunel <johnkevinpaunel@gmail.com>
2019-09-04 18:08:45 +0800

    updated the call center page address call

--- resources/views/SuperAdmin/lgu/lgu.blade.php
+++ resources/views/SuperAdmin/lgu/lgu.blade.php
@@ -1,9 +1,26 @@
-@extends('layouts.adminDashboardTemplate')
+@extends('layouts.admin_template')
 
 @section('title')
     Super Admin | LGU
 @endsection
-
+@section('variable_menu')
+    <li><a href="{{url('/super-admin/dashboard')}}"><i class="fa fa-dashboard"></i> <span>Dashboard</span></a></li>
+    <li><a href="{{url('employee')}}"><i class="fa fa-user-plus"></i> <span>Employee</span></a></li>
+    <li class="treeview">
+        <a href="#">
+            <i class="fa fa-bank"></i><span>Roles</span>
+            <span class="pull-right-container">
+                <i class="fa fa-angle-left pull-right"></i>
+            </span>
+            <ul class="treeview-menu">
+                <li><a href="{{url('/super-admin/roles')}}">View Roles</a></li>
+                <li><a href="/super-admin/permissions">View Permissions</a></li>
+            </ul>
+        </a>
+    </li>
+    <li><a href="{{url('/super-admin/callCenter')}}"><i class="fa fa-phone-square"></i> <span>Call Center</span></a></li>
+    <li class="active"><a href="{{url('/super-admin/lgu')}}"><i class="fa fa-bank"></i> <span>LGUs</span></a></li>
+@endsection
 @section('main_content')
 
 @endsection
