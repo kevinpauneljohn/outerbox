c85d32c3737424c2ac642157b8f9617427ea3841
iamdashjei <jovitopangan123@gmail.com>
2019-12-02 17:38:24 +0800

    Ticket change

--- resources/views/Employee/Agent/tickets.blade.php
+++ resources/views/Employee/Agent/tickets.blade.php
@@ -47,9 +47,9 @@
 
                     <td>{{date_format(date_create($ticket->date_reported),"d/M/Y H:i:s")}}</td>
                     <td>{{ \App\Http\Controllers\AgentPageController::get_requested_date($ticket->app_response) }}</td>
-                    <td>s</td>
+                    <td>{{ $ticket->duration_before_agent_handled_call != null ? \App\Http\Controllers\Ticket\TicketController::calculate_seconds_of_dates($ticket->date_reported,$ticket->time_handled). "s" : "" }}</td>
                     {{-- <td>{{ $dateTime->dateDifftwoDates($ticket->date_reported,$ticket->time_handled) }}</td> --}}
-                    <td>{{ $ticket->duration_before_agent_handled_call != null ? \App\Http\Controllers\Ticket\TicketController::calculate_seconds_of_dates($ticket->time_handled,$ticket->duration_before_agent_handled_call) . "s" : "" }}</td>
+                    {{-- <td>{{ $ticket->duration_before_agent_handled_call != null ? \App\Http\Controllers\Ticket\TicketController::calculate_seconds_of_dates($ticket->time_handled,$ticket->duration_before_agent_handled_call) . "s" : "" }}</td> --}}
                     <td>{{ $ticket->call_duration != null ? $ticket->call_duration . " s" : "" }}</td>
                     <td>{{ $ticket->duration_until_agent_transfer_request != null ? \App\Http\Controllers\Ticket\TicketController::calculate_seconds_of_dates($ticket->duration_before_agent_handled_call,$ticket->duration_until_agent_transfer_request). "s" : "" }}</td>
                     <td>{{ $ticket->duration_before_agent_handled_call != null ? \App\Http\Controllers\Ticket\TicketController::calculate_seconds_of_dates($ticket->time_handled,$ticket->duration_until_agent_transfer_request). "s" : "" }}</td>
